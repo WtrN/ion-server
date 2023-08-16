dbb30f0e6fc78b82dc1b2188e25054c0c596919f
WtrN <nishi.w.136@gmail.com>
2023-08-16 17:36:33 +0900

    Basic認証の情報をdotenvに移管

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IonModule } from './ion/ion.module';
 import { BasicAuthModule } from './basic_auth/auth.module';
 
 @Module({
-  imports: [IonModule, BasicAuthModule],
+  imports: [
+    IonModule,
+    BasicAuthModule,
+    ConfigModule.forRoot({
+      envFilePath: ['config/.env.develop'],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/basic_auth/basic.strategy.ts
+++ src/basic_auth/basic.strategy.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { BasicStrategy } from 'passport-http';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
@@ -5,12 +6,15 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 // Basic認証
 @Injectable()
 export class BasicAuthStrategy extends PassportStrategy(BasicStrategy) {
-    constructor() {
-      super();
-    }
-  
-    validate(username: string, password: string): boolean {
-      if (username === 'admin' && password === 'develop') return true;
-      else throw new UnauthorizedException();
-    }
+  constructor(private readonly configService: ConfigService) {
+    super();
   }
+
+  validate(username: string, password: string): boolean {
+    const basicAuthUser = this.configService.get<string>('BASIC_AUTH_USER');
+    const basicAuthPass = this.configService.get<string>('BASIC_AUTH_PASSWORD');
+
+    if (username === basicAuthUser && password === basicAuthPass) return true;
+    else throw new UnauthorizedException();
+  }
+}
